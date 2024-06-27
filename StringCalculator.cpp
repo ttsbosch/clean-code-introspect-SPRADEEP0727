891934492d134282055012e37f9907ada1cced79
SPRADEEP0727 <75067284+SPRADEEP0727@users.noreply.github.com>
2024-06-27 15:07:52 +0530

    Update StringCalculator.cpp

--- StringCalculator.cpp
+++ StringCalculator.cpp
@@ -1,9 +1,8 @@
 #include "StringCalculator.h"
 
 int StringCalculator::add(string input){
-        if(input == "") && (input == NULL)
-                {
-                        return 0;
-                }
+ if (input.empty() || input == "0") {
+        return 0;
+    }
         return 1;
 }
